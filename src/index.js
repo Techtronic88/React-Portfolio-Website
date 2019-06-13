c9fe630098180efe6c3542d478701153260b241d
DoanThomas88 <38048770+Techtronic88@users.noreply.github.com>
2019-06-14 00:49:35 +1000

    Add files via upload
    Changes made to this commit
    - Changed the navigation bar positions so they appear in correct order.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { Router, Switch, Route } from "react-router-dom";
 import { createBrowserHistory } from 'history';
 import MusicMaster from "./projects/music-master/components/App"
+import WeatherCity from "./projects/weather-app/components/App"
 import App from "./components/App";
 import Jokes from "./components/Jokes";
 import Header from "./components/Header";
 import "./index.css";
+var PORT = process.env.PORT || 3000;
 
 
 ReactDOM.render(
@@ -15,7 +17,8 @@ ReactDOM.render(
             <Route exact path="/" render={() => <Header><App /></Header>} />
             <Route path="/jokes" render={() => <Header><Jokes /></Header>} />
             <Route path="/music-master" render={() => <Header><MusicMaster /></Header>} />
-            
+            <Route path="/weather-app" render={() => <Header><WeatherCity /></Header>} />
+            <Route path="/getaway-meditation" render={() => <Header><WeatherCity /></Header>} />
         </Switch>
     </Router>,
     document.getElementById('root')
