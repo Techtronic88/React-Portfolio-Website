fcc575816217852c933220abf445baf6e54ad8ff
TomDoan <38048770+TomDoan88@users.noreply.github.com>
2019-07-19 02:02:33 +1000

    Add files via upload
    - Change data and new links associated with social media.

--- src/data/socialProfile.js
+++ src/data/socialProfile.js
@@ -1,31 +1,41 @@
-import email_icon from "../assets/email_icon.png";
-import github_icon from "../assets/github_icon.png";
-import linkedin_icon from "../assets/linkedin_icon.png";
-import twitter_icon from "../assets/twitter_icon.png";
+import email_icon from "../assets/email.png";
+import github_icon from "../assets/github.png";
+import linkedin_icon from "../assets/linkedin.png";
+import twitter_icon from "../assets/twitter.png";
+import blog from "../assets/blog.png";
 
 const SOCIALPROFILES = [
-    {
-     id: 1,   
-     image: email_icon,
-     name: "Email",
-     link: "mailto:Thomasdoan88@gmail.com"   
-    }, 
-    {
-     id: 2,   
-     image: github_icon,
-     name: "Github",
-     link: "https://github.com/Techtronic88"},
+  {
+    id: 1,
+    image: email_icon,
+    name: "Email",
+    link: "mailto:Thomasdoan88@gmail.com"
+  },
+  {
+    id: 2,
+    image: github_icon,
+    name: "Github",
+    link: "https://github.com/TomDoan88"
+  },
 
-    {
-     id:3,   
-     image: linkedin_icon,
-     name: "Linkedin",
-     link: "https://www.linkedin.com/in/thomas-doan-07636b151/"},
-     {
-     id: 4,    
-     image: twitter_icon,
-     name: "Twitter",
-     link: "https://twitter.com/thomasdoan88"}
+  {
+    id: 3,
+    image: linkedin_icon,
+    name: "Linkedin",
+    link: "https://linkedin.com/in/tom-doan"
+  },
+  {
+    id: 4,
+    image: twitter_icon,
+    name: "Twitter",
+    link: "https://twitter.com/thomasdoan88"
+  },
+  {
+    id: 5,
+    image: blog,
+    name: "BLog",
+    link: "https://tomddoan.wordpress.com"
+  }
 ];
 
 export default SOCIALPROFILES;
